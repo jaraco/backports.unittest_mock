569692d8379f8e18c5208776de602ee7f404c030
Jason R. Coombs <jaraco@jaraco.com>
2016-10-14 22:05:04 -0400

    Try just pinning to the 1.13

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup_params = dict(
 		],
 	},
 	setup_requires=[
-		'setuptools_scm>=1.9,!=1.13.1,!=1.14.0',
+		'setuptools_scm==1.13.0',
 	] + wheel,
 	classifiers=[
 		"Development Status :: 5 - Production/Stable",
