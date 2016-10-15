e3c70641297209ec46fb31f7dcaccea4dc3cb3ab
Jason R. Coombs <jaraco@jaraco.com>
2016-10-14 20:56:46 -0400

    Declare support for Python 2.6 and 3.2

--- setup.py
+++ setup.py
@@ -31,7 +31,9 @@ setup_params = dict(
 	install_requires=[
 	],
 	extras_require={
-		':python_version=="2.7"': [
+		':python_version=="2.7" or '
+		'python_version="2.6" or '
+		'python_version="3.2"': [
 			"mock",
 		],
 	},
@@ -45,8 +47,10 @@ setup_params = dict(
 		"Development Status :: 5 - Production/Stable",
 		"Intended Audience :: Developers",
 		"License :: OSI Approved :: MIT License",
+		"Programming Language :: Python :: 2.6",
 		"Programming Language :: Python :: 2.7",
 		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.2",
 		"Programming Language :: Python :: 3.3",
 		"Programming Language :: Python :: 3.4",
 		"Programming Language :: Python :: 3.5",
